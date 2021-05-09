20ed11a2503787abf06436d1e90913690e3eb675
Rich <richarda20@hotmail.com>
2021-05-09 21:05:43 +0200

    tweaks

--- core/templatetags/random_testimonial_tags.py
+++ core/templatetags/random_testimonial_tags.py
@@ -7,4 +7,4 @@ register = template.Library()
 @register.simple_tag()
 def get_random_testimonial():
     count = Testimonial.objects.all().filter(locale_id=1).count()
-    return Testimonial.objects.all().filter(locale_id=1)[randint(0, count - 1)]
+    return Testimonial.objects.all().filter(locale_id=1)[randint(0, count - 1)].localized
