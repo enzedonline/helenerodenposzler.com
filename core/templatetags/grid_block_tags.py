a01269bef4269e931b0782af0472b254ade0e8d4
Rich <richarda20@hotmail.com>
2021-05-17 17:42:06 +0200

    column fixes

--- core/templatetags/grid_block_tags.py
+++ core/templatetags/grid_block_tags.py
@@ -11,14 +11,18 @@ def two_column_layout(breakpoint, column_layout, horizontal_padding):
         breakpoint = ''
         layout['horizontal_padding'] = 'px-' + str(horizontal_padding)
         layout['breakpoint_pixels'] = '0px'
+        layout['pre_breakpoint_bottom_pad'] = ''
     else:
         layout['horizontal_padding'] = 'px' + breakpoint + '-' + str(horizontal_padding)
         if breakpoint == '-sm':
             layout['breakpoint_pixels'] = '575px'
-        if breakpoint == '-md':
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-sm-0'
+        elif breakpoint == '-md':
             layout['breakpoint_pixels'] = '767px'
-        if breakpoint == '-lg':
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-md-0'
+        else:
             layout['breakpoint_pixels'] = '991px'
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-lg-0'
 
     layout['left'] = breakpoint + (('-' + layout['left']) if layout['left'] else '')
     layout['right'] = breakpoint + (('-' + layout['right']) if layout['right'] else '')
@@ -33,14 +37,18 @@ def three_column_layout(breakpoint, column_layout, horizontal_padding):
         breakpoint = ''
         layout['horizontal_padding'] = 'px-' + str(horizontal_padding)
         layout['breakpoint_pixels'] = '0px'
+        layout['pre_breakpoint_bottom_pad'] = ''
     else:
         layout['horizontal_padding'] = 'px' + breakpoint + '-' + str(horizontal_padding)
         if breakpoint == '-sm':
             layout['breakpoint_pixels'] = '575px'
-        if breakpoint == '-md':
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-sm-0'
+        elif breakpoint == '-md':
             layout['breakpoint_pixels'] = '767px'
-        if breakpoint == '-lg':
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-md-0'
+        else:
             layout['breakpoint_pixels'] = '991px'
+            layout['pre_breakpoint_bottom_pad'] = 'pb-3 pb-lg-0'
 
     layout['left'] = breakpoint + (('-' + layout['left']) if layout['left'] else '')
     layout['centre'] = breakpoint + (('-' + layout['centre']) if layout['centre'] else '')
