75957a30142fde0625fcb598aea8476d7fd6ed94
Richard Allen <richard@enzedonline.com>
2021-12-14 13:00:15 +0100

    Menu update

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from wagtail.core.models import Page
 
 register = template.Library()
 
@@ -18,7 +19,6 @@ def robots(context):
         '<meta name="robots" content="index, follow, archive, imageindex, odp, snippet, translate, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />'
     )
 
-
 @register.simple_tag(takes_context=True)
 def get_context_var_or_none(context, name):
     dicts = context.dicts  # array of dicts
@@ -31,3 +31,13 @@ def get_context_var_or_none(context, name):
 @register.simple_tag
 def print_template_value(value):
     print(value)
+
+@register.simple_tag()
+def trans_page_from_slug(slug, specific=False):
+    try:
+        if specific:
+            return Page.objects.live().filter(slug=slug).first().specific.localized
+        else:
+            return Page.objects.live().filter(slug=slug).first().localized
+    except:
+        return Page.objects.none()
