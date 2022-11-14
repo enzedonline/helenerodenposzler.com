3bc5f66169f86a977ce459390b36162272ce1d30
Richard Allen <richard@enzedonline.com>
2022-11-14 17:04:36 +0100

    drop unused CompanyLogo

--- menu/models.py
+++ menu/models.py
@@ -424,18 +424,3 @@ class SubMenuItem(MenuItem):
         unique_together = ('translation_key', 'locale')
  
 
-@register_snippet
-class CompanyLogo(models.Model):
-    name = models.CharField(max_length=250)
-    logo = models.ForeignKey(
-        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
-    )
-
-    panels = [
-        FieldPanel("name", classname="full"),
-        FieldPanel("logo"),
-    ]
-
-    def __str__(self):
-        return self.name
-
