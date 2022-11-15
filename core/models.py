7367a798e1fdd4ff716a425095ac31ec6a4bfaf4
Richard Allen <richard@enzedonline.com>
2022-11-15 19:18:53 +0100

    Add page_type & services to SEO_Page

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.db import models
+from django.forms.widgets import CheckboxSelectMultiple
 from django.utils.translation import gettext_lazy as _
+from modelcluster.fields import ParentalManyToManyField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.models import Page
 from wagtailcaptcha.models import WagtailCaptchaEmailForm
 from wagtailmetadata.models import WagtailImageMetadataMixin
+
+from core.edit_handlers import LocalizedSelectPanel
 
 
 def get_image_model_string():
@@ -32,6 +36,24 @@ class SEOPageMixin(WagtailImageMetadataMixin, models.Model):
                      If Search Description is left blank, this text will be used on searh results and link previews.")
     )
 
+    page_type = models.CharField(
+        max_length=25,
+        choices=[
+            ("WebPage", "Web Page"),
+            ("AboutPage", "About Page"),
+            ("ContactPage", "Contact Page"),
+            ("ItemPage", "Item Page")
+        ],
+        default="WebPage",
+        verbose_name=_("Structured Data Page Type")
+    )
+
+    services = ParentalManyToManyField(
+        'site_settings.ServiceType',
+        verbose_name=_("Services offered on this page"),
+        blank=True
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel('summary'),
     ]
@@ -41,8 +63,17 @@ class SEOPageMixin(WagtailImageMetadataMixin, models.Model):
             FieldPanel('slug'),
             FieldPanel('search_image'),
             FieldPanel('seo_title'),
-            FieldPanel('show_in_menus'),
             FieldPanel('search_description'),
+            FieldPanel('page_type'),
+            MultiFieldPanel(
+                [
+                    LocalizedSelectPanel(
+                        'services', 
+                        widget_class=CheckboxSelectMultiple, 
+                        ),
+                ],
+                heading = _("Services"),
+            ),
         ], _('Common page configuration')),
     ]
 
