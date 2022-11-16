17e66e014a948736df1353438dccbe6e80876c57
Richard Allen <richard@enzedonline.com>
2022-11-16 11:15:08 +0100

    RegexPanel > core

--- menu/edit_handlers.py
+++ menu/edit_handlers.py
@@ -270,22 +270,3 @@ class SubMenuFieldPanel(FieldPanel):
                 menu_list = menu_list.exclude(id=parent_menu_id)
             return [('', '------')] + list(menu_list.values_list('id','title'))
 
-class RegexPanel(FieldPanel):
-
-    def __init__(self, field_name, pattern, *args, **kwargs):
-        self.pattern = pattern
-        super().__init__(field_name, *args, **kwargs)
-
-    def clone_kwargs(self):
-        kwargs = super().clone_kwargs()
-        kwargs.update(
-            pattern=self.pattern,
-        )
-        return kwargs
-        
-    class BoundPanel(FieldPanel.BoundPanel):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)           
-            self.form.fields[self.field_name].__setattr__('pattern', self.panel.pattern)
-
-        field_template_name = "edit_handlers/regex_panel_field.html"
