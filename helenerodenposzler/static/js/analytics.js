1dee7296223c32d95f4432f2b8da8298bf3dc6a6
Richard Allen <richard@enzedonline.com>
2022-11-20 12:09:32 +0100

    analytics: mv to js file, block dev/editor events

---
+++ helenerodenposzler/static/js/analytics.js
@@ -0,0 +1,22 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-D4N5E5D93L");
+
+// Clarity tracking code for https://helenerodenposzler.com/
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "76ppf671sr");
