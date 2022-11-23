a17b5a3a99bb6383ac3ea9bba63552629a543a6e
Richard Allen <richard@enzedonline.com>
2022-11-23 17:22:47 +0100

    change to core.views.sitemap

--- helenerodenposzler/urls.py
+++ helenerodenposzler/urls.py
@@ -2,24 +2,26 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views import defaults as default_views
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from core.views import RobotsView, sitemap
 from menu.views import set_language_from_url
 from search import views as search_views
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.contrib.sitemaps.views import sitemap
-from wagtail import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
-from core.views import RobotsView
-from django.views import defaults as default_views
+
 
 def trigger_error(request):
     division_by_zero = 1 / 0
     
+
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     re_path(r'^robots\.txt$', RobotsView.as_view(), name='robots'),
-    re_path(r'^sitemap.xml$', sitemap),
+    re_path(r'^sitemap.xml$', sitemap, name='sitemap'),
     path('sentry-debug/', trigger_error),
     path('lang/<str:language_code>/', set_language_from_url, name='set_language_from_url'),
 ]
