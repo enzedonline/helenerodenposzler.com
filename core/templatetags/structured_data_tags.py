278dc9f1d0245e5b79fedfc5474ed901f832e898
Richard Allen <richard@enzedonline.com>
2022-12-19 18:01:22 +0100

    error handling in structured data

--- core/templatetags/structured_data_tags.py
+++ core/templatetags/structured_data_tags.py
@@ -14,12 +14,19 @@ def organisation_name():
     
 @register.simple_tag()
 def get_google_thumbnails(img):
-    return {
-        'tn1x1': img.get_rendition('thumbnail-500x500|format-png'),
-        'tn4x3': img.get_rendition('thumbnail-500x375|format-png'),
-        'tn16x9': img.get_rendition('thumbnail-500x281|format-png'),
-    }
-    
+    try:
+        return {
+            'tn1x1': img.get_rendition('thumbnail-500x500|format-png'),
+            'tn4x3': img.get_rendition('thumbnail-500x375|format-png'),
+            'tn16x9': img.get_rendition('thumbnail-500x281|format-png'),
+        }
+    except:
+        return {
+            'tn1x1': '',
+            'tn4x3': '',
+            'tn16x9': '',
+        }
+            
 @register.simple_tag()
 def get_social_media_sameas():
     same_as = []
