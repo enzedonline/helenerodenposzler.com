942ddb4d1934b9191e0c28ae0106f3634bb1b551
Richard Allen <richard@enzedonline.com>
2023-03-15 14:57:05 +0100

    reformat

--- core/views.py
+++ core/views.py
@@ -8,21 +8,30 @@ from wagtail.models import Page, Site
 
 env = os.environ.copy()
 
+
 class RobotsView(TemplateView):
 
-    content_type = 'text/plain'
+    content_type = "text/plain"
 
     def get_template_names(self):
-        return 'robots.txt'
+        return "robots.txt"
+
 
 def sitemap(request):
     site = Site.find_for_request(request)
     root_page = Page.objects.defer_streamfields().get(id=site.root_page_id)
     urlset = []
-    for locale_home in root_page.get_translations(inclusive=True).live().defer_streamfields().specific():
+    for locale_home in (
+        root_page.get_translations(inclusive=True)
+        .live()
+        .defer_streamfields()
+        .specific()
+    ):
         if locale_home.search_engine_index:
             urlset.append(locale_home.sitemap_urls)
-        for child_page in locale_home.get_descendants().live().defer_streamfields().specific():
+        for child_page in (
+            locale_home.get_descendants().live().defer_streamfields().specific()
+        ):
             if child_page.search_engine_index:
                 urlset.append(child_page.sitemap_urls)
     try:
@@ -30,20 +39,22 @@ def sitemap(request):
     except:
         pass
     try:
-        last_modified = max([x['lastmod'] for x in urlset])
+        last_modified = max([x["lastmod"] for x in urlset])
     except Exception as e:
         # either urlset is empty or lastmod fields not present, set last modified to now
-        print(f"\n{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}\n") 
+        print(
+            f"\n{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}\n"
+        )
         last_modified = datetime.now()
 
     return TemplateResponse(
-        request, 
-        template='sitemap.xml', 
-        context={'urlset': urlset},
-        content_type='application/xml',
+        request,
+        template="sitemap.xml",
+        context={"urlset": urlset},
+        content_type="application/xml",
         headers={
-            "X-Robots-Tag": "noindex, noodp, noarchive", 
+            "X-Robots-Tag": "noindex, noodp, noarchive",
             "last-modified": http_date(last_modified.timestamp()),
             "vary": "Accept-Encoding",
-            }
-        )
+        },
+    )
